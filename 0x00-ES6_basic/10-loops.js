d20022690ef6095f4d508ade6e6a9c98b09bfbf3
se <Yoseph Tamirat>
2024-06-26 20:39:05 +0300

    for loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
-
 export default function appendToEachArrayValue(array, appendString) {
-for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
-}
+  const arr = [];
+  for (const idx of array) {
+      const value = idx;
+      arr.push(appendString + value);
+  }
 
-return array;
+  return arr;
 }
