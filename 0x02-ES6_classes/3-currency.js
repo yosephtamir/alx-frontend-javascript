bb70f0e1564494249c82ec0cf4ce7fbbb08cac46
se <Yoseph Tamirat>
2024-07-01 17:28:35 +0300

    place exchenge

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -6,8 +6,8 @@ export default class Currency {
     if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
     }
-    this._name = name;
     this._code = code;
+    this._name = name;
   }
 
   get name() {
