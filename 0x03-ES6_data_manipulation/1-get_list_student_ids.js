ec006ec1fe782d4ba0b00ba8daa3c6564fe5d839
se <Yoseph Tamirat>
2024-07-02 14:28:11 +0300

    checked

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,11 +2,5 @@ export default function getListStudentIds(arr) {
   if (!(Array.isArray(arr))) {
     return [];
   }
-  const len = arr.length;
-  const newArr = [];
-  for (let i = 0; i < len; i += 1) {
-    console.log(arr[i].location.includes('San Francisco'))
-    newArr.push(arr[i].id);
-  }
-  return newArr;
+  return arr.map((obj) => obj.id);
 }
